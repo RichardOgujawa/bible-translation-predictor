9832b98b97d57649d3fd8f10d348896135df74a8
Richard Ogujawa <richardogujawa@Richards-MacBook-Pro.local>
2024-02-20 18:18:37 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bible Translation Predictor 📖
+
+This is an API which which has a model under the hood which is used to make predictions about which bible translation a particular verse is being read from.
+<br/>
+
+## Why? 🤷🏾‍♂️
+
+The Bible began as a collection of writings largely inscribed in three languages: **Hebrew, Aramaic and Greek.** Over time the Scriptures were translated into over **700 languages** to accommodate believers in different nations, and within each language potentially exists a handful, if not several hundred translations. **In the Enlgish language alone ”approximately 500 ... versions** of the complete Bible have been written over the last few hundred years. Of those, nearly **100 versions are currently in print**.” ([Faith Founded on Fact](http://faithfoundedonfact.com/how-many-versions-bible/), 6)
+
+Popular versions include ASV (American Standard Version), BSB (Berean Study Bible) and KJV (King Kames Version), however even given the awareness of these popular translations, **it’s not always easy to identify which one is being used at any given time**. Along with this, although some people who preach or read from the Bible preface their reading of the verse or passage with the translation they’re reading from, this often is not the case. **As such people often find themselves either scrambling to find the version as the speaker reads on, or they simply acquiesce to following along with a version that communicates a similar meaning but with different phraseology.**
+<br/>
+
+## Goal 🎯
+
+The goal of this project is to **remove the friction between the people trying to follow a reading or preaching and the reader or preacher**, allowing the reader to read whichever translation suits them without the risk of people potentially struggling to follow along.
+<br/>
+
+## Credits
+
+- [API Bible](https://scripture.api.bible/docs) - Thanks to API Bible for the data necessary to train the model.
---
+++ main.py
@@ -0,0 +1,68 @@
+"""# IMPORTS"""
+from flask import Flask
+from dotenv import dotenv_values
+import requests
+import json
+import pandas as pd
+
+"""# SETUP"""
+## Load in environment variables
+config = dotenv_values(".env") 
+API_KEY=config['API_TOKEN']
+
+## Setup application
+app = Flask(__name__)
+
+"""# MODEL"""
+HEADERS = {'api-key': API_KEY}
+
+## Fetch data for bible translations
+def get_bible_versions(see_keys=False):
+    url="https://api.scripture.api.bible/v1/bibles"
+    # Get the response object from the fetch
+    response = requests.request("GET", url, headers=HEADERS)
+    # Convert the text to a json
+    data = json.loads(response.text)['data']
+    if see_keys:
+        print(data[0].keys())
+    df = pd.DataFrame(columns=['id', 'abbreviation', 'name', 'language'], data=data)
+    # Only return language name in language column, not entire dictionary
+    df['language'] = df['language'].apply(lambda x: x['name'])
+    # Return only english bibles
+    eng_df = df[df['language'] == 'English']
+    return eng_df
+
+## Fetch data for chapters in the bible translation
+def get_books_in_bible(bibleId, see_keys=False):
+    url = f"https://api.scripture.api.bible/v1/bibles/{bibleId}/books"
+    # Get the response object from the fetch
+    response = requests.request("GET", url, headers=HEADERS)
+    # Convert the text to a json
+    data = json.loads(response.text)['data']
+    if see_keys:
+        print(data[0].keys())
+    df = pd.DataFrame(columns=['id', 'nameLong'], data=data)
+    return df
+    
+bible_versions_df = get_bible_versions()
+print(bible_versions_df)
+
+BSB_id = bible_versions_df[bible_versions_df['name'] == 'Berean Standard Bible']['id'].values[0]
+# Get the books in the BSB
+bible_books_df = get_books_in_bible(BSB_id, see_keys=True)
+print(bible_books_df)
+# ## Fetch data for verses in the bible translations chapter
+# bible_verse_api_url="https://api.scripture.api.bible/v1/bibles/bibleId/chapters/chapterId/verses"
+# print(response.text)
+
+
+
+## Get data from bible api
+
+## Homepage
+# @app.route('/')
+# def home():
+#     return config['API_TOKEN']
+
+# if __name__=='__main__':
+#     app.run(debug=True)
